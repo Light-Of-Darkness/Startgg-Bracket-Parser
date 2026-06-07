e6f00a08be4c6cc6be5ba5af4ea4f807bc7421c5
LoDE <no@email.com>
2026-06-07 11:11:34 -0400

    refactoring query to use for loops, should now work for NvN brackets

--- graph_ql_query.py
+++ graph_ql_query.py
@@ -26,19 +26,16 @@ allSets = getSets(eventID)
 players = {}
 setDict = {}
 
-spinner = Halo(text='Getting all sets from the event...', spinner='dots')
-spinner.start()
 for fSet in allSets['event']['sets']['nodes']:
     #print(fSet)
     #populate the dictionary of all sets in the event, where the set ID is the key and the set data is the value
     setDict[fSet['id']] = fSet['slots']
-    print(setDict[fSet['id']])
+    #print(setDict[fSet['id']])
     for entrant in fSet['slots']:
         if entrant['entrant']['id'] not in players:
             #add entry to players dictionary, where player ID is the key and name is the value
             players[str(entrant['entrant']['id'])] = entrant['entrant']['name']
             
-spinner.stop()
 #Create and populate a dictionary of player scores, where the key is their start.gg id
 dictPlayerScores = {}
 
@@ -51,30 +48,44 @@ spinner.start()
 
 for _set in setDict:
     #id is the id for the player within the set, entrant id is the id for the player in start.gg (idk why this shit is different)
-    setPlayers = {
-        setDict[_set][0]['id'] : setDict[_set][0]['entrant']['id'],
-        setDict[_set][1]['id'] : setDict[_set][1]['entrant']['id']
-    }
+    #setPlayers = {
+    #    setDict[_set][0]['id'] : setDict[_set][0]['entrant']['id'],
+    #    setDict[_set][1]['id'] : setDict[_set][1]['entrant']['id']
+    #}
+    setPlayers = {}
+    for set in setDict[_set]:
+        setPlayers.update({set['id']:set['entrant']['id']})
     
-    result = getSetScore(_set)
-    player1 = ( result['set']['slots'][0]['id'], result['set']['slots'][0]['standing']['stats']['score']['value'] )
-    player2 = ( result['set']['slots'][1]['id'], result['set']['slots'][1]['standing']['stats']['score']['value'] )
+    results = getSetScore(_set)
+    _players = []
+    #player1 = ( result['set']['slots'][0]['id'], result['set']['slots'][0]['standing']['stats']['score']['value'] )
+    #player2 = ( result['set']['slots'][1]['id'], result['set']['slots'][1]['standing']['stats']['score']['value'] )
+    for result in results['set']['slots']:
+        if result['standing']['stats']['score']['value'] is None or result['standing']['stats']['score']['value'] == -1:
+            _players.append((result['id'], 0))
+        else:
+            _players.append((result['id'], result['standing']['stats']['score']['value']))
     
     #Check if win count is null or -1 (-1 is DQ, null means their opponent DQed)
-    if player1[1] is None or player1[1] == -1:
-        playerList = list(player1)
-        playerList[1] = 0
-        player1 = tuple(playerList)
-    #Same check for player 2
-    if player2[1] is None or player2[1] == -1:
-        playerList = list(player2)
-        playerList[1] = 0
-        player2 = tuple(playerList)
         
-    dictPlayerScores[str(setPlayers[player1[0]])] += player1[1]
-    dictPlayerScores[str(setPlayers[player2[0]])] += player2[1]
+        
+    #if player1[1] is None or player1[1] == -1:
+    #    playerList = list(player1)
+    #    playerList[1] = 0
+    #    player1 = tuple(playerList)
+    #Same check for player 2
+    #if player2[1] is None or player2[1] == -1:
+    #    playerList = list(player2)
+    #    playerList[1] = 0
+    #    player2 = tuple(playerList)
+        
+    #dictPlayerScores[str(setPlayers[player1[0]])] += player1[1]
+    #dictPlayerScores[str(setPlayers[player2[0]])] += player2[1]
+    for player in _players:
+        dictPlayerScores[str(setPlayers[player[0]])] += player[1]
 
 spinner.stop()
 
+print('Total games won')
 for playerID in dictPlayerScores:
     print('{} : {}'.format(players[playerID], dictPlayerScores[playerID]))
